dc162029fde7197f96127ba7979367d400d5c399
Denis Ermolin <denis.ermolin@matic.network>
2021-08-23 21:06:39 +0700

    fix: hardhat config

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -6,18 +6,13 @@ const { MNEMONIC, API_KEY } = process.env
 
 const config = {
   defaultNetwork: 'hardhat',
-  mainnet: {
-    chainId: 1,
-    gas: 'auto',
-    accounts: { mnemonic: MNEMONIC }
-  },
-  hardhat: {
-    chainId: 1,
-    gas: 8e6,
-    forking: {
-      url: `https://mainnet.infura.io/v3/${API_KEY}`
-    },
-    accounts: { mnemonic: MNEMONIC }
+  networks: {
+    mainnet: {
+      url: `https://mainnet.infura.io/v3/${API_KEY}`,
+      chainId: 1,
+      gas: 'auto',
+      accounts: { mnemonic: MNEMONIC }
+    }
   },
   solidity: '0.7.6',
   spdxLicenseIdentifier: {
