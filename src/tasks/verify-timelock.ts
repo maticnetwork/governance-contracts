2e9879338de104db7fa7ebe804d3243a2e44bd06
Jaynti Kanani <jdkanani@gmail.com>
2021-09-17 15:08:32 +0400

    fix: console results

--- src/tasks/verify-timelock.ts
+++ src/tasks/verify-timelock.ts
@@ -3,7 +3,7 @@ import { TIMELOCK_DELAY, MULTISIG } from '../contants'
 
 task('verify:timelock', 'Verifies Timelock governance', async(args, hre) => {
   await hre.run('verify:verify', {
-    address: '0x933dcf4b58d3ae8fb0cde6e10e22f6c0e062cdc1',
+    address: '0xCaf0aa768A3AE1297DF20072419Db8Bb8b5C8cEf',
     contract: 'contracts/TimeLock.sol:Timelock',
     constructorArguments: [
       TIMELOCK_DELAY,
